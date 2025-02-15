455f005df808bf34d642097fd97ec4f8810f1916
abbeysamson05 <ajayiabiodunsamson05@gmail.com>
2025-02-15 10:25:43 +0100

    Started working on authentication screen

---
+++ src/constant/index.tsx
@@ -0,0 +1,96 @@
+import {
+  FinancialReportIcon,
+  InventoryManagementIcon,
+  OrderIcon,
+  ProductIcon,
+  ReportIcon,
+  StoreManagementIcon,
+  UserIcon,
+} from "../../public/icons";
+import { ROUTES } from "./routes";
+
+export const adminSidebarList = [
+  {
+    id: 1,
+    sidebar: "Admin Management",
+    icon: <UserIcon />,
+    child: [
+      {
+        sidebar: "Admins",
+        href: ROUTES.ADMIN.SIDEBAR.ADMINS,
+      },
+      {
+        sidebar: "Roles",
+        href: ROUTES.ADMIN.SIDEBAR.ROLES,
+      },
+      {
+        sidebar: "Permissions",
+        href: ROUTES.ADMIN.SIDEBAR.PERMISSIONS,
+      },
+    ],
+  },
+  {
+    id: 2,
+    sidebar: "Customers",
+    icon: <UserIcon />,
+    child: [
+      {
+        sidebar: "Business Owner",
+        href: ROUTES.ADMIN.SIDEBAR.BUSINESSOWNERS,
+      },
+      {
+        sidebar: "Individual",
+        href: ROUTES.ADMIN.SIDEBAR.INDIVIDUALS,
+      },
+    ],
+  },
+  {
+    id: 3,
+    sidebar: "Products",
+    href: ROUTES.ADMIN.SIDEBAR.PRODUCTS,
+    icon: <ProductIcon />,
+  },
+  {
+    id: 4,
+    sidebar: "Orders",
+    icon: <OrderIcon />,
+    href: ROUTES.ADMIN.SIDEBAR.ORDERS,
+  },
+  {
+    id: 5,
+    sidebar: "Reports",
+    icon: <ReportIcon />,
+    child: [
+      {
+        sidebar: "Customers Report",
+        href: ROUTES.ADMIN.SIDEBAR.CUSTOMERSREPORT,
+      },
+      {
+        sidebar: "Business Owner",
+        href: ROUTES.ADMIN.SIDEBAR.BUSINESSOWNERSREPORT,
+      },
+      {
+        sidebar: "Individual",
+        href: ROUTES.ADMIN.SIDEBAR.INDIVIDUALREPORT,
+      },
+    ],
+  },
+  {
+    id: 6,
+    sidebar: "Store Management",
+    icon: <StoreManagementIcon />,
+    href: ROUTES.ADMIN.SIDEBAR.STOREMANAGEMENT,
+  },
+  {
+    id: 7,
+    sidebar: "Inventory Management",
+    icon: <InventoryManagementIcon />,
+    href: ROUTES.ADMIN.SIDEBAR.INVEMTORYMANAGEMENT,
+  },
+  {
+    id: 8,
+    sidebar: "Financial Reports",
+    icon: <FinancialReportIcon />,
+    href: ROUTES.ADMIN.SIDEBAR.FINANCIALREPORTS,
+  },
+];
---
+++ src/constant/routes.ts
@@ -0,0 +1,22 @@
+export const ROUTES = {
+  ADMIN: {
+    SIDEBAR: {
+      DASHBOARD: "/admin",
+      ADMINS: "/admin/admins",
+      ROLES: "/admin/roles",
+      PERMISSIONS: "/admin/permissions",
+      BUSINESSOWNERS: "/admin/business-owners",
+      INDIVIDUALS: "/admin/individuals",
+      PRODUCTS: "/admin/products",
+      ORDERS: "/admin/orders",
+      CUSTOMERSREPORT: "/admin/customer-reports",
+      BUSINESSOWNERSREPORT: "/admin/business-owner-report",
+      INDIVIDUALREPORT: "/admin/individual-reports",
+      STOREMANAGEMENT: "/admin/store-management",
+      INVEMTORYMANAGEMENT: "/admin/inventory-management",
+      FINANCIALREPORTS: "/admin/financial-reports",
+      NOTIFICATIONS: "/admin/notifications",
+      SETTINGS: "/admin/setting",
+    },
+  },
+};
