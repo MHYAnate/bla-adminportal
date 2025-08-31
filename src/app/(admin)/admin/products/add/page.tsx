2abbc986d794fcd5984205568933e843150f19fa
Mohammed-Hayatudeen Yusuf Anate <mhibnyusufanate@gmail.com>
2025-08-31 09:13:51 +0100

    fix:removeFontSchemaExportFromAddProduct

--- src/app/(admin)/admin/products/add/page.tsx
+++ src/app/(admin)/admin/products/add/page.tsx
@@ -833,7 +833,7 @@ const optionSchema = z.discriminatedUnion("profitType", [
 ]);
 
 // 4️⃣ Final form schema with top-level refinement
-export const formSchema = z
+const formSchema = z
   .object({
     name: z.string().min(3, "Product name must be at least 3 characters"),
     description: z.string().min(10, "Description must be at least 10 characters"),
