34cf1e6f89ee12423568516bb639d396f7cb211d
Mohammed-Hayatudeen Yusuf Anate <mhibnyusufanate@gmail.com>
2025-09-08 17:30:01 +0100

    removeFilterPriority

--- src/app/(admin)/admin/support/components/support-filters.tsx
+++ src/app/(admin)/admin/support/components/support-filters.tsx
@@ -66,11 +66,11 @@ const SupportFilters: React.FC<SupportFiltersProps> = ({
             list={categoryOptions}
           />
 
-          <SelectFilter
+          {/* <SelectFilter
             setFilter={(value: string) => onFilterChange("priority", value)}
             placeholder="Filter by Priority"
             list={priorityOptions}
-          />
+          /> */}
         </div>
       </div>
     </div>
